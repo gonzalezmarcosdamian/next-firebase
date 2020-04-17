111e5db0327e6bdfbb60f95e65b7fee3d6eca870
Marcos damian Gonzalez <ingonzalezdamian@gmail.com>
2020-04-17 15:38:13 -0300

    finalizado

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5H-a0F4dBN01hnwfqe-5QXf7XTgq1MiM",
+  authDomain: "product-hunt-86f84.firebaseapp.com",
+  databaseURL: "https://product-hunt-86f84.firebaseio.com",
+  projectId: "product-hunt-86f84",
+  storageBucket: "product-hunt-86f84.appspot.com",
+  messagingSenderId: "842960233786",
+  appId: "1:842960233786:web:a8643d15219ef839d6dc03"
+};
+
+export default firebaseConfig;
